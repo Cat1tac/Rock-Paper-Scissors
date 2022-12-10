981ba222867f9e6f49ba200ee321537f82f3a2eb
Cat1tac <waynepowell235@gmail.com>
2022-12-10 18:45:13 -0500

    Created a working output for game

--- javascript.js
+++ javascript.js
@@ -2,19 +2,34 @@ const getPlayerChoice = prompt("Enter Rock, or Scissors");
 
 const getComputerChoice = Math.floor(Math.random() * 3) + 1;
 console.log(getComputerChoice);
+var computerSelection = '';
 
 if (getComputerChoice === 1){
-    console.log('Rock');
+    computerSelection = 'Rock';
 } else if (getComputerChoice === 2) {
-    console.log('Paper');
+    computerSelection = 'Paper';
 } else {
-    console.log('Scissors');
+    computerSelection = 'Scissors';
 }
 
-const playerSelection = playerSelection.toUpperCase();
+const playerSelection = getPlayerChoice.toLowerCase();
 
 function playRound(playerSelection, computerSelection){
-
+    if (playerSelection == 'rock' && computerSelection == 'Scissors'){
+        return 'You Win! Rock beats Scissors';
+    } else if (playerSelection == 'rock' && computerSelection == 'Paper'){
+        return 'You Lose! Paper beats Rock';
+    } else if (playerSelection == 'paper' && computerSelection == 'Rock'){
+        return 'You Win! Paper beats Rock';
+    } else if (playerSelection == 'paper' && computerSelection == 'Scissors'){
+        return 'You Lose! Scissors beats Paper';
+    } else if (playerSelection == 'scissors' && computerSelection == 'Paper'){
+        return 'You Win! Scissors beats Paper';
+    } else if (playerSelection == 'scissors' && computerSelection == 'Rock'){
+        return 'You Lose! Rock beats Scissors';
+    } else {
+        return 'Draw';
+    }
 }
 
 console.log(playRound(playerSelection, computerSelection));
